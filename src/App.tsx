e510d37603cd3f300ed751a4686da91595da0a09
Hao2005 <23662046@kthcm.edu.vn>
2025-11-13 02:21:05 +0000

    dong_bo_13.11

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,55 @@ import Layout from "./Layout";
 // @ts-ignore
 import Trang1 from "./Trang1";
 // @ts-ignore
-import Chitietsanpham from "./Chitietsanpham";
+import ProductDetail from "./ProductDetail";
 // @ts-ignore
 import Trang2 from "./Trang2";
 // @ts-ignore
 import ListProducts from "./ListProducts";
-// @ts-ignore
-import ListProducts_SP from "./ListProducts_SP";
-// @ts-ignore
-import ProductDetail from "./ProductDetail";
 
 // @ts-ignore
-import ListSanPham from "./ListSanPham";
+import ListProducts_SP from "./ListProducts_SP";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//@ts-ignore
+import LoginPage from "./LoginPage";
+//@ts-ignore
+import LogoutPage from "./LogoutPage";
+//@ts-ignore
+import ProtectedRoute from "./ProtectedRoute";
+//@ts-ignore
+import ListProducts_SP_Admin from "./ListProducts_SP_Admin";
 
 const App = () => {
   //return <Layout />;
   return (
     <BrowserRouter>
       <Routes>
+        {/* ✅ Layout chung cho toàn bộ hệ thống */}
         <Route path="/" element={<Layout />}>
+          {/* Trang chính (cho người dùng vãng lai) */}
           <Route index element={<ListProducts_SP />} />
-          <Route path="detail/:id" element={<ProductDetail />} />
           <Route path="trang1" element={<Trang1 />} />
-          <Route path="sanpham/:id" element={<Chitietsanpham />} />
-          <Route path="Trang2" element={<Trang2 />} />
-          <Route path="ListSanPham" element={<ListSanPham />} />
+          <Route path="trang2" element={<Trang2 />} />
+          <Route path="sanpham/:id" element={<ProductDetail />} />
+          {/* <Route path="detail/:id" element={<ProductDetail />} /> */}
+
+          {/* ✅ Trang đăng nhập (nằm trong Layout) */}
+          <Route path="login" element={<LoginPage />} />
+
+          {/* ✅ Trang đăng xuất */}
+          <Route path="logout" element={<LogoutPage />} />
+
+          {/* ✅ Trang quản trị (nằm trong Layout, chỉ Admin truy cập) */}
+          <Route
+            path="admin/products"
+            element={
+              <ProtectedRoute>
+                <ListProducts_SP_Admin />
+              </ProtectedRoute>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
